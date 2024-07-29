8a8da62c6bca624720932e46154b922aa8328855
Mihai Pruna <141741004+mihai-pruna@users.noreply.github.com>
2024-07-29 18:16:06 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Set the database credentials and backup file name
-DB_HOST="localhost"
-DB_USER="your_username"
-DB_PASSWORD="your_password"
+#DB_HOST="localhost"
+#DB_USER="your_username"
+#DB_PASSWORD="your_password"
+
+DB_HOST="${DB_HOST"
+DB_USER="${DB_USER}"
+DB_PASSWORD="${DB_PASSWORD}"
 DB_NAME="your_database"
 BACKUP_FILE="mysql_backup_$(date +\%Y-\%m-\%d).sql"
 
