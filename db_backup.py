8225e2a9b31cac1c5ecead9533e03f9a4d923284
Mihai Pruna <141741004+mihai-pruna@users.noreply.github.com>
2024-07-30 16:35:17 +0300

    Update db_backup.py

--- db_backup.py
+++ db_backup.py
@@ -9,7 +9,8 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
-BACKUP_FILE = f"mysql_backup_{now.strftime('%Y-%m-%d_%H-%M-%S')}.sql"
+BACKUP_FILE="mysql_backup_$(date +'%Y-%m-%d_%H-%M-%S').sql"
+#BACKUP_FILE = f"mysql_backup_{now.strftime('%Y-%m-%d_%H-%M-%S')}.sql"
 print(BACKUP_FILE)
 
 cmd = f"mysqldump -h {DB_HOST} -u {DB_USER} -p{DB_PASSWORD} {DB_NAME} > {BACKUP_FILE}"
